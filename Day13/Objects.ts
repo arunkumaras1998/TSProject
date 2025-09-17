bda54a481be94e0a29e514f94fc813c465f8c914
Arun Kumar A S <arun1998.ag12@gmail.com>
2025-09-17 03:08:01 -0700

    added Objects file

---
+++ Day13/Objects.ts
@@ -0,0 +1,20 @@
+// Objects - object contains properties and behaviour
+// Objects contains Variables and Methods
+// Object is a collection of Key and value pairs.
+
+// Ex - Employee -- name, desig, sal, dep
+// bonus(), getEmpDeatils(), setEmpDetails()
+
+// Ex - Student -- name, sid, grade, dep
+// getStdDeatils(), setStdDetails()
+
+/* Different ways to create an objects in TS/JS
+1. using 'Object' Type - Directly define the value for variables (TS\JS)
+2. Inline Type Object -  We also define the datatype of the keys (TS\JS)
+3. Using Type aliases (TS\JS)
+4. Using the classes (JS ES16/TS)
+*/
+
+
+// 1. using 'Object' Type - Directly define the value for variables (TS\JS)
+// The TypeScript 'object' type represents all values that are not in primitive types
